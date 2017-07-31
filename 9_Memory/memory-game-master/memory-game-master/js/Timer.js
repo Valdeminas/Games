a809af6133f07032d1b60e628c9b1c86d0fca918
Gediminas Ziezys <vilkabrolis@gmail.com>
2017-07-31 19:30:14 +0300

    Initial

---
+++ 9_Memory/memory-game-master/memory-game-master/js/Timer.js
@@ -0,0 +1,33 @@
+var s;
+var m;
+var intervalID;
+var condition;
+
+function startTimer(param){
+  s=0;
+  m=0;
+  condition=param;
+intervalID=setInterval(startTime, 1000);
+}
+
+function startTime() {
+document.getElementById('output').innerHTML =
+checkTime(m) + ":" + checkTime(s);
+s=s+1;
+if(s>condition){
+  clearInterval(intervalID);
+  var score = 0
+  var message = "Message";
+
+  document.getElementById('memory--end-game-message').textContent = message;
+  document.getElementById('memory--end-game-score').textContent =
+      'Score: ' + score + ' / 100';
+
+  document.getElementById("memory--end-game-modal").classList.toggle('show');
+}
+
+}
+function checkTime(i) {
+if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+return i;
+}
