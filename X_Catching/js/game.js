27833f8a2f2b911b224dd8059245e028f77b19ff
Valdeminas <Vilkabrolis@gmail.com>
2017-08-07 12:53:13 +0300

    Update game.js

--- X_Catching/js/game.js
+++ X_Catching/js/game.js
@@ -98,8 +98,8 @@ function create_game() {   
     var i;       
     var apple;       
     var dateNow = Date.now();       
-    c.width = window.innerWidth;  
-    c.height = window.innerHeight;       
+    c.width = window.innerWidth-30;  
+    c.height = window.innerHeight-30;       
     for (i = 0; i < apples.length; i++) {           
       apple = apples[i];           
       if (dateNow > apple.delay) {               
